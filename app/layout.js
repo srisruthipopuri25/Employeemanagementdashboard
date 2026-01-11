dca6654a70baa35d93866f88013be55236cf5446
sruthi <srisruthipopuri@gmail.com>
2026-01-11 23:02:02 +0530

    title changes

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import "./globals.css";
 
+export const metadata = {
+title: 'Employee Management Dashboard',
+description: 'Employee Management Dashboard with Next.js and Zustand',
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
